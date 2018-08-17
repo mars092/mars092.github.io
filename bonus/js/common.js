910237389504b319a744dec8ab05815639698131
mars092 <mars0092@mail.ru>
2018-08-17 20:22:42 +0300

    asdad

---
+++ bonus/js/common.js
@@ -0,0 +1,25 @@
+$(function () {
+
+	// Custom JS
+	$("#tel1").mask("+7 (999) 999-99-99");
+	$("#tel2").mask("+7 (999) 999-99-99");
+
+});
+
+$(document).on('click', '.spoiler-trigger', function (e) { e.preventDefault(); $(this).toggleClass('active'); $(this).parent().find('.spoiler-block').first().slideToggle(300); })
+
+$(document).ready(function () {
+	$("#menu").on("click", "a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id = $(this).attr('href'),
+
+			//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+});
